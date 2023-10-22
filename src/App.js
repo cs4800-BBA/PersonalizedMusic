738ac2ff9a7e531d41b2b06f118e3b212671413f
Benjamin Dinh <benjadinh@gmail.com>
2023-10-22 13:22:26 -0700

    TestApp 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import { useState } from "react";
+import './App.css';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+    </div>
+  );
 }
 
 export default App;
