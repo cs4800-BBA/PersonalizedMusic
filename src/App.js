32a0f277262ddb502e6cb0e3ea3e227bc285634e
Benjamin Dinh <benjadinh@gmail.com>
2023-10-22 14:00:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const value = 'Test Working';
-  return <div>Hello {value}</div>;
+  const value = 'Test is Working';
+  return <div>That is great the  {value}</div>;
 }
 
 export default App;
