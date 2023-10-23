1ae972c7613852708bc92296e484c833d32522a4
Benjamin Dinh <benjadinh@gmail.com>
2023-10-22 21:11:51 -0700

    1.24 Working

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 import React, { useState, useEffect } from "react";
-
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
-
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
-
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
-
   useEffect(() => {
     searchMovies("Batman");
   }, []);
-
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-
     setMovies(data.Search);
   };
-
   return (
     <div className="app">
-      <h1>MovieLand</h1>
-
+      <h1>Personalized Music Recommender</h1>
       <div className="search">
         <input
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for movies"
+          placeholder="Search for songs"
         />
         <img
           src={SearchIcon}
@@ -37,7 +29,6 @@ const App = () => {
           onClick={() => searchMovies(searchTerm)}
         />
       </div>
-
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
@@ -46,11 +37,10 @@ const App = () => {
         </div>
       ) : (
         <div className="empty">
-          <h2>No movies found</h2>
+          <h2>No songs found</h2>
         </div>
       )}
     </div>
   );
 };
-
 export default App;
