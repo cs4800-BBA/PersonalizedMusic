686c02f1b10c236054199487f7d1034d6faf1ac8
Benjamin Dinh <benjadinh@gmail.com>
2023-10-25 21:13:53 -0700

    Should Work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import SongCard from "./SongCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
-const API_URL = "https://personal-music-recommendation.azurewebsites.net/api";
+
 const App = () => {
   
   const [searchTerm, setSearchTerm] = useState("");
@@ -13,11 +13,18 @@ const App = () => {
     searchSong("Firework");
   }, []);
 
+
+// Get Song from Backend
   const searchSong = async (song) => {
-    const response = await fetch(`${API_URL}recommendation?code=dkS5_6Zm8E-ElF4KzKlwPwZTDm-0_5d2_Q-Re5afhl-yAzFu-Ak5rg==&song=${song}`);
-    const data = await response.json();
-    setSong(data.Search);
+    const API_URL = "https://personal-music-recommendation.azurewebsites.net/api/recommendation";
+    const functionKey = "BiLtlWfdvS4NmIH_Y9_xDnCT1Cs5rOLoLWvenK88PQW8AzFuDX25TA==";
+
+    //const response = await fetch(`${API_URL}?code=${functionKey}&song=${song}`) ;
+    //const data = await response.json();
+    //setSong(data.Search);
   };
+
+
   return (
     <div className="app">
       <h1>Personalized Music Recommender</h1>
@@ -30,7 +37,7 @@ const App = () => {
         <img
           src={SearchIcon}
           alt="search"
-          onClick={() => searchSongs(searchTerm)}
+          onClick={() => searchSong(searchTerm)}
         />
       </div>
       {songs?.length > 0 ? (
@@ -41,7 +48,7 @@ const App = () => {
         </div>
       ) : (
         <div className="empty">
-          <h2>No songs found</h2>
+          <h2>"No Songs Found</h2>
         </div>
       )}
     </div>
