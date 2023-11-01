fbe14ba5c8287d335bd112264fa91bdcf6d33964
Benjamin Dinh <benjadinh@gmail.com>
2023-10-31 18:41:21 -0700

    Song Format Implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-//import SongCard from "./SongCard";
+import SongCard from "./SongCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
+
 
 const App = () => {
   
@@ -19,7 +20,7 @@ const App = () => {
     
     const API_URL = "https://personal-music-recommendation.azurewebsites.net/api/recommendation";
     const functionKey = "BiLtlWfdvS4NmIH_Y9_xDnCT1Cs5rOLoLWvenK88PQW8AzFuDX25TA==";
-    const response = await fetch(`${API_URL}?code=${functionKey}&song=${song}`) ;
+    const response = await fetch(`${API_URL}?code=${functionKey}&limit=10&song=${song}`) ;
     const data = await response.json();
    
    if (Array.isArray(data)) {
@@ -47,7 +48,7 @@ const App = () => {
               searchSong(searchTerm);
             }
           }}
-          placeholder="Search for songs"
+          placeholder="Search for songs..."
         />
         <img
           src={SearchIcon}
@@ -57,17 +58,13 @@ const App = () => {
       </div>
 
       <div>
-      <h2>Recommended Tracks</h2>
-      <ul>
-        {recommendedTracks.map((track, index) => (
-          <li key={index} className="track-item">
-            <p>Name: {track.name}</p>
-            <p>Artists: {track.artists.join(', ')}</p>
-            <p>Listen on Spotify: <a href={track.external_url} target="_blank" rel="noopener noreferrer">Listen</a></p>
-            <img src={track.images[0].url} alt="Track" />
-          </li>
-        ))}
-      </ul>
+          <div className="container">
+            {recommendedTracks.map((track, index) => (
+              <SongCard song={track} key={index} />
+               ))}
+
+
+          </div>
     </div>
       
     </div>
