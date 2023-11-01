6023a04bae68740b5a193e9e826656864ffbfc3c
Benjamin Dinh <benjadinh@gmail.com>
2023-10-31 19:11:34 -0700

    Update to show original song

--- src/App.js
+++ src/App.js
@@ -7,20 +7,22 @@ import "./App.css";
 const App = () => {
   
   const [searchTerm, setSearchTerm] = useState("");
-  //const [songs, setSong] = useState([]);
+  const [ogSong, setSong] = useState([]);
   const [recommendedTracks, setRecommendedTracks] = useState([]);
   
   useEffect(() => {
+    searchOgSong("Firework");
     searchSong("Firework");
+    
   }, []);
 
 
-// Get Song from Backend
+// Get Song Recommendations from Backend
   const searchSong = async (song) => {
     
     const API_URL = "https://personal-music-recommendation.azurewebsites.net/api/recommendation";
     const functionKey = "BiLtlWfdvS4NmIH_Y9_xDnCT1Cs5rOLoLWvenK88PQW8AzFuDX25TA==";
-    const response = await fetch(`${API_URL}?code=${functionKey}&limit=10&song=${song}`) ;
+    const response = await fetch(`${API_URL}?code=${functionKey}&limit=11&song=${song}`) ;
     const data = await response.json();
    
    if (Array.isArray(data)) {
@@ -35,6 +37,26 @@ const App = () => {
   };
 }
 
+// Get Original Song from Backend
+const searchOgSong = async (song) => {
+    
+  const API_URL = "https://personal-music-recommendation.azurewebsites.net/api/search";
+  const functionKey = "dkS5_6Zm8E-ElF4KzKlwPwZTDm-0_5d2_Q-Re5afhl-yAzFu-Ak5rg==";
+  const response = await fetch(`${API_URL}?code=${functionKey}&limit=1&q=${song}`) ;
+  const data = await response.json();
+ 
+ if (Array.isArray(data)) {
+  const ogSong = data.map(ogSongs => ({
+    name: ogSongs.name,
+    artists: ogSongs.artists,
+    external_url: ogSongs.external_url,
+    images: ogSongs.images
+  }));
+
+  setSong(ogSong);
+};
+}
+
 
   return (
     <div className="app">
@@ -46,6 +68,7 @@ const App = () => {
             onKeyDown={(e) => {
               if (e.keyCode === 13) {
               searchSong(searchTerm);
+              searchOgSong(searchTerm);
             }
           }}
           placeholder="Search for songs..."
@@ -53,12 +76,14 @@ const App = () => {
         <img
           src={SearchIcon}
           alt="search"
-          onClick={() => searchSong(searchTerm)}
+          onClick={() => {searchSong(searchTerm); searchOgSong(searchTerm);}}
         />
       </div>
-
       <div>
           <div className="container">
+            {ogSong.map((track, index) => (
+              <SongCard song={track} key={index} />
+               ))}
             {recommendedTracks.map((track, index) => (
               <SongCard song={track} key={index} />
                ))}
